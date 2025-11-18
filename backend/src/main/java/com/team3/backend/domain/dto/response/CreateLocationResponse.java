9c8a29e6bfcace20297128cd94f31894cc3ac461
Namyeon Kim <ouroboros0519@gmail.com>
2025-11-18 11:41:48 +0900

    feat : Location Response DTO 구현

---
+++ backend/src/main/java/com/team3/backend/domain/dto/response/CreateLocationResponse.java
@@ -0,0 +1,11 @@
+package com.team3.backend.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateLocationResponse {
+
+    private Long locationId;
+}
---
+++ backend/src/main/java/com/team3/backend/domain/dto/response/LocationListResponse.java
@@ -0,0 +1,15 @@
+package com.team3.backend.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LocationListResponse {
+
+    private Long locationId;
+    private String placeName;
+    private Double latitude;
+    private Double longitude;
+    private Boolean pinned;
+}
