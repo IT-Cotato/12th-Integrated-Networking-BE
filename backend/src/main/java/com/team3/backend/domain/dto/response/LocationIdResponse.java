b82cc0c61639379ca9d1b07e0ab91f6efce0347a
Namyeon Kim <ouroboros0519@gmail.com>
2025-11-18 19:11:59 +0900

    feat : Location Response DTO 수정

--- backend/src/main/java/com/team3/backend/domain/dto/response/LocationIdResponse.java
+++ backend/src/main/java/com/team3/backend/domain/dto/response/LocationIdResponse.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class CreateLocationResponse {
+public class LocationIdResponse {
 
     private Long locationId;
 }
--- backend/src/main/java/com/team3/backend/domain/dto/response/LocationListResponse.java
+++ backend/src/main/java/com/team3/backend/domain/dto/response/LocationListResponse.java
@@ -1,10 +1,12 @@
 package com.team3.backend.domain.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class LocationListResponse {
 
     private Long locationId;
