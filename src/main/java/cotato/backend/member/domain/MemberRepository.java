167bc4274bdea9b8115c7edce1109818f59baf01
GiminKim <gimin1463@naver.com>
2025-11-22 22:58:10 +0900

    feat: Member 도메인 추가

---
+++ src/main/java/cotato/backend/member/domain/MemberRepository.java
@@ -0,0 +1,10 @@
+package cotato.backend.member.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByProviderTypeAndProviderId(ProviderType providerType, String providerId);
+
+}
